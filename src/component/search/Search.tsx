4dd2c22e2b3dbb989f79f0ce688fbc95edfa1f64
LOI Bui Huu (VTI.D8) <buihuuloi1402@gmail.com>
2022-08-16 17:43:38 +0700

    axios

---
+++ src/component/search/Search.tsx
@@ -0,0 +1,10 @@
+import "./search.scss"
+
+function Search(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Search;
---
+++ src/config/readApi.tsx
@@ -0,0 +1,8 @@
+import * as request from "./axios";
+
+const readApi = () => {
+  // call API
+
+  return request.get(`movies`);
+};
+export default readApi;
