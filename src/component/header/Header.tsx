f0754201efffb49c94690610d034047ad59fb065
LOI Bui Huu (VTI.D8) <buihuuloi1402@gmail.com>
2022-08-18 10:25:39 +0700

    notsearch

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,7 +1,33 @@
 import Logo from "../../assets/img/logo.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { IData } from "../../config/types";
+import { getMovies } from "../../config/axios";
 
 function Header() {
+  const [search, setSearch] = useState("");
+  const [itemMovie, setItemMovie] = useState<IData[]>([]);
+  const [data, setData] = useState<IData[]>([]);
+  useEffect(() => {
+    const handleGetFilm = async () => {
+      const getFilm = await getMovies();
+      setData(getFilm);
+    };
+    handleGetFilm();
+  }, []);
+  const handleSearch = () => {
+    const setFilm = data.filter((item) =>
+      item.snippet.title.toLocaleLowerCase().includes(search)
+    );
+    setItemMovie(setFilm);
+  };
+  console.log(itemMovie[0]);
+
+  const handleKey = (e: { key: string }) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <div className="flex w-full bg-[#202020] justify-between items-center px-[16px] border-[1px] border-solid border-[#ffffff1a] fixed">
       <Link to="/">
@@ -12,11 +38,17 @@ function Header() {
       </Link>
       <div className="search  text-[#ffffffe0] border-[1px] border-solid border-[#ffffff1a]">
         <input
+          value={search}
+          onKeyDown={handleKey}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
           className="h-[40px] w-[540px] bg-[#121212] outline-0 px-[6px] py-[2px]"
           type="text"
           placeholder="Tìm kiếm"
         />
-        <button className="bg-[#313131] h-[40px] w-[64px]">
+        <button
+          onClick={handleSearch}
+          className="bg-[#313131] h-[40px] w-[64px]"
+        >
           <i className="ti-search"></i>
         </button>
       </div>
